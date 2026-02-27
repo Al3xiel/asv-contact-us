da10176165bb5ec93756a44a923b64d58e0da0d1
Al3xiel <gabriel280104casas@gmail.com>
2026-02-27 00:22:19 -0500

    feat(pages): add contact us page

---
+++ src/app/shared/pages/contact-us-1/contact-us-1.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us-1',
+  imports: [],
+  templateUrl: './contact-us-1.html',
+  styleUrl: './contact-us-1.css',
+})
+export class ContactUs1 {}
